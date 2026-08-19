74c173d92f52ab1e135dffc9473ce0573a332900
raoxikui <raoxikui@qq.com>
2026-08-19 13:57:24 +0800

    优化

--- database/migrations/2026_05_06_201917_create_biz_access_dispatch_table.php
+++ database/migrations/2026_05_06_201917_create_biz_access_dispatch_table.php
@@ -23,13 +23,14 @@ return new class extends Migration
             $table->foreignId('access_user_id')->nullable()->index()->comment('门禁用户id');
             $table->foreignId('access_device_id')->nullable()->index()->comment('门禁设备id');
             $table->foreignId('access_permission_id')->nullable()->index()->comment('门禁权限id');
-            $table->integer('enterprise3_id')->nullable()->comment('门禁权限码');
-            $table->string('auth_model', 16)->nullable()->default('days')->comment('授权类型:每天days、工作日workdays、自定义日期custom');
+            $table->integer('enterprise_id')->nullable()->comment('门禁权限码');
+            $table->string('auth_model', 24)->nullable()->default('days')->comment('授权类型:每天days、工作日workdays、自定义日期custom');
             $table->text('auth_date')->nullable()->comment('授权日期');
             $table->time('start_time')->nullable()->comment('有效期开始时间');
             $table->time('end_time')->nullable()->comment('有效期结束时间');
             $table->text('exclude_date')->nullable()->comment('排除日期');
             $table->smallInteger('sort')->nullable()->default(100)->comment('排序/优先级');
+            $table->string('user_type', 24)->nullable()->comment('用户类型：worker员工,student学生,patriarch家长,visitor访客');
             $table->smallInteger('state')->nullable()->default(0)->comment('状态：0-待发，-1下发中，1成功，2失败');
             $table->string('remark')->nullable()->comment('备注');
             $table->string('module', 32)->nullable();
